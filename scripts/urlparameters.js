f36e3f6c00503b191b262d4f99cecef3f5a4a524
maanemann <hans.august@gmail.com>
2023-03-02 14:09:15 +0100

    Add har fået den til at vise destination billedet!

--- scripts/urlparameters.js
+++ scripts/urlparameters.js
@@ -1,8 +1,12 @@
 const params = new URLSearchParams(window.location.search);
+const place = document.querySelector('.place');
 
 fetch('./data/' + params.get('id') + '.json')
     .then((response) => response.json())
     .then(data => {
         console.log(data.image);
+        place.innerHTML = `
+            <img src="img/${data.image}" alt="Picture of the place">
+        `;
     })
 ;
